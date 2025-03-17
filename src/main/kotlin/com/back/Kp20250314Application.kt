8f0d1f595aaad9f6462543d014f5260ff80c2dc7
wjwj910 <wjwj910@gmail.com>
2025-03-18 00:19:07 +0900

    first commit

---
+++ src/main/kotlin/com/back/Kp20250314Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kp20250314Application
+
+fun main(args: Array<String>) {
+    runApplication<Kp20250314Application>(*args)
+}
---
+++ src/test/kotlin/com/back/Kp20250314ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kp20250314ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
